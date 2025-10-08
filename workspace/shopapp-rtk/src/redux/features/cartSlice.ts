bab2c11b3dbf825a4ac182b5264475341a038134
Banuprakash <banuprakashc@yahoo.co.in>
2025-10-08 12:52:55 +0530

    rtk

--- workspace/shopapp-rtk/src/redux/features/cartSlice.ts
+++ workspace/shopapp-rtk/src/redux/features/cartSlice.ts
@@ -19,10 +19,19 @@ const cartSlice = createSlice({
   initialState,
   reducers: {
     addToCart: (state, action: PayloadAction<CartItem>) => {
-
+        state.items.push(action.payload);
+        state.total += action.payload.price ;
+        state.quantity += 1;
     },
     increment: (state, action: PayloadAction<number>) => {
-
+        state.items.forEach(item => {
+            if(item.id === action.payload) {
+                item.qty += 1;
+                item.amount = item.price * item.qty;
+                state.total = state.items.map(item => item.amount).reduce((a,b) => a + b, 0);
+                state.quantity += 1;
+            }
+        })
     },
     clearCart: (state) => {
         state.items = [];
@@ -31,6 +40,11 @@ const cartSlice = createSlice({
     }
   }
 });
+
+// dispatch(addToCart({id: 2, title: "", ..}))
+// dispatch(clearCart())
+// dispatch(increment(3));
+// no longer you use like dispatch({type:'ADD_TO_CART', payload: {...}})
 
 export const { addToCart, increment, clearCart } = cartSlice.actions;
 
